7d977cfd6af3bb33f079e922f65e91b9e234d2a6
niirjhor <niirjhor@gmail.com>
2021-09-02 22:28:56 +0600

    finish work

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-// Error handling
 const errorDiv = document.getElementById('error-result');
 const searchAreaText = document.getElementById('search-area');
 const showResultText = document.getElementById('result-show-card');
+const resultFound = document.getElementById('result-found');
 // search field text additon
 
 const searchButton = () => {
     const searchArea = searchAreaText.value;
     searchAreaText.value = '';
+    //clear previous result
     showResultText.innerHTML = '';
-    //Error Handling
+    //Error Handling for empty search
     if (searchArea === '') {
-        errorDiv.innerText = "Search field can't be empty";
+        errorDiv.innerText = "Search field can't be empty!!!!!";
+        showResultText.innerHTML = '';
+        resultFound.textContent = '';
+
     }
     else {
         const url = `http://openlibrary.org/search.json?q=${searchArea}`;
-
         fetch(url)
             .then(res => res.json())
             .then(data => resultShow(data))
@@ -24,10 +27,9 @@ const searchButton = () => {
 
 
 const resultShow = books => {
-
-    // Showing total search result count 
-    const resultFound = document.getElementById('result-found');
+    //clear result count
     resultFound.textContent = '';
+    // Showing total search result count
     const p = document.createElement('p');
     p.innerHTML = `
         <p>Total ${books.numFound} Results</p>        
@@ -36,25 +38,37 @@ const resultShow = books => {
 
     //Error handling for not found book name
     if (books.numFound === 0) {
-        errorDiv.innerText = "Search not found";
+        errorDiv.innerText = "Search not found!!!!!";
+        resultFound.textContent = '';
     }
     // Result of search
-    for (const book of books.docs) {
+    books.docs.forEach(book => {
         const div = document.createElement('div');
         div.classList.add('col');
+        // Nullable operator for author & Publisher name to manage the no applicable author array
+        const authorName = !book.author_name ? "Author Name Is Not Given" : book.author_name[0];
+        const publisherName = !book.publisher ? "Publisher Name Is Not Given" : book.publisher[0];
+        let imgSrc = ''
+        if (book.cover_i === undefined) {
+            imgSrc = '/no-preview.jpg'
+        }
+        else {
+            imgSrc = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+        }
         div.innerHTML = `
         <div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+            <img src="${imgSrc}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h3 class="book-title">${book.title}</h3>
-                <h5 class="book-author">${book.author_name[0]}</h5>
-                <p class="book-publisher text-green">${book.publisher}</p>
+                <h5 class="book-author">${authorName}</h5>
+                <p class="book-publisher text-green">${publisherName}</p>
                 <p class="book-publish-year">${book.first_publish_year}</p>
             </div>
         </div>
     `;
         showResultText.appendChild(div);
-
     }
+    )
 }
+
 
